48e96f96f22d83294f6ddc424fd42d8b64510914
Fernando Cruz <cruzfernandomartin94@gmail.com>
2022-06-08 09:01:50 -0300

    Agregado del desafío Counter. Creación de modelo de card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import ItemListContainer from "./components/ItemListContainer"
+import { useState } from 'react'
 import "./styles.css"
 
 
 function App() {
+
+  const [productStock, setProductStock] = useState(5)
+
+  const [counter, setCounter] = useState(0)
+
+  const onAdd = () => {
+    if (productStock > 0){
+      setCounter(prevCounter => prevCounter + 1)
+      setProductStock(prevProductStock => prevProductStock - 1)
+    }
+  }
+
+  const onSubstract = () =>{
+    if(counter > 0){
+      setCounter(prevCounter => prevCounter - 1)
+      setProductStock(prevProductStock => prevProductStock + 1 )
+    }
+  }
+
   return (
     <>
       <Header />
 
       <Hero />
       
-      <ItemListContainer greeting="Bienvenidos a los productos" />
+      <ItemListContainer 
+        greeting="Bienvenidos a los productos"
+        onAdd={onAdd}
+        onSubstract={onSubstract}
+        counter={counter}
+        stock={productStock}
+        />
+      
     </>
 
   );
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,21 @@
 export default function ItemListContainer(props){
     return(
-        <div>
-            <h2>{props.greeting}</h2>
-            <p>Info about the product</p>
-            <p>Price</p>
+        <div className="itemListContainer">
+            <div className='card'>
+                <img src="https://picsum.photos/200/300" alt="product"/>
+                <div className="card--text">
+                    <h2>{props.greeting}</h2>
+                    <p>Info about the product</p>
+                    <p>Price</p>
+                    <div>
+                        <p>Stock: {props.stock}</p>
+                        <p> You're taking: {props.counter} </p>
+                        <button onClick={props.onAdd}>+</button>
+                        <button onClick={props.onSubstract}>-</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
